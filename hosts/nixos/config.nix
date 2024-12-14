50c933c2c79bcc3f0e15a3b57fee8c05403a210a
installer <installer@gmail.com>
2024-12-14 09:49:25 -0500

    fix Anime-Girl1.png

--- hosts/nixos/config.nix
+++ hosts/nixos/config.nix
@@ -54,7 +54,7 @@ in
   # Styling Options
   stylix = {
     enable = true;
-    image = ../../config/wallpapers/Anime-Girl1.jpg;
+    image = ../../config/wallpapers/Anime-Girl1.png;
     # base16Scheme = {
     #   base00 = "232136";
     #   base01 = "2a273f";
