671136e7046c9ae4ad667a4c66a3cc8a2ecaa1c8
installer <installer@gmail.com>
2024-12-15 10:05:43 -0500

    simplified treesitter

--- config/nvim/plugins/treesitter.lua
+++ config/nvim/plugins/treesitter.lua
@@ -1,25 +1,20 @@
-require("nvim-treesitter.configs").setup{
+require('nvim-treesitter.configs').setup {
     ensure_installed = {},
-    ignore_install = {},
-    auto_install = true,
-    sync_install = false,
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-        disable = function()
-            return vim.b.large_buf
-        end,
-    },
+
+    auto_install = false,
+
+    highlight = { enable = true },
+
+    indent = { enable = true },
+
     incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "<enter>",
-            node_incremental = "<enter>",
-            node_decremental = "<BS>",
-            scope_incremental = false,
-        },
-    },
-    indent = {
-        enable = false,
-    },
-    }
+                enable = true,
+                keymaps = {
+                    init_selection = "<enter>",
+                    node_incremental = "<enter>",
+                    node_decremental = "<BS>",
+                    scope_incremental = false,
+                    -- scope_incremental = "<c-s>",
+                },
+            },
+}
