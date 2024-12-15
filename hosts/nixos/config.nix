4fca34eb48e0df3eb63fd9e976a3735c19caf349
installer <installer@gmail.com>
2024-12-15 18:42:08 -0500

    changed stylix wallpaper

--- hosts/nixos/config.nix
+++ hosts/nixos/config.nix
@@ -54,7 +54,7 @@ in
   # Styling Options
   stylix = {
     enable = true;
-    image = ../../config/wallpapers/zaney-wallpaper.jpg;
+    image = ../../config/wallpapers/Akiakane-2.jpg;
     # base16Scheme = {
     #   base00 = "232136";
     #   base01 = "2a273f";
