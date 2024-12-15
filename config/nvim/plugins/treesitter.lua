1d5dd341f1d0dca6e3b803e97de8ed483d12fc91
installer <installer@gmail.com>
2024-12-15 10:00:40 -0500

    attempt 4

--- config/nvim/plugins/treesitter.lua
+++ config/nvim/plugins/treesitter.lua
@@ -22,33 +22,6 @@ require("nvim-treesitter.configs").setup({
     indent = {
         enable = false,
     },
-    textobjects = {
-        select = {
-            enable = false,
-            lookahead = true,
-            keymaps = {
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
-                ["ab"] = "@block.outer",
-                ["ib"] = "@block.inner",
-                ["a#"] = "@condition.outer",
-                ["i#"] = "@condition.inner",
-            },
-        },
-        move = {
-            enable = false,
-            set_jumps = true,
-            goto_next_start = {
-                ["]m"] = "@function.outer",
-                ["]#"] = "@conditional.outer",
-                ["]l"] = "@loop.outer",
-                ["]b"] = "@block.outer",
-            },
-            -- etc...
-        },
-        -- other textobjects configurations...
     },
 })
 require("nvim-treesitter.install").prefer_git = true
