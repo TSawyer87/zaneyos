c484887b55e50ea6b90119d465ec69968a6eff47
installer <installer@gmail.com>
2024-12-16 18:54:59 -0500

    mini-files

--- config/nvim/plugins/mini-files.lua
+++ config/nvim/plugins/mini-files.lua
@@ -1,18 +1,7 @@
-require('mini.files').setup()
-  keys = {
-    {
-      "<leader>fm",
-      function()
-        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
-      end,
-      desc = "Open mini.files (Directory of Current File)",
-    },
-    {
-      "<leader>fM",
-      function()
-        require("mini.files").open(vim.uv.cwd(), true)
-      end,
-      desc = "Open mini.files (cwd)",
-    },
-  },
  
+  require('mini.files').setup({ 
+  keys = {
+    { "<leader>fm", function() require("mini.files").open(vim.api.nvim_buf_get_name(0), true) end, desc = "Open mini.files (Directory of Current File)" },
+    { "<leader>fM", function() require("mini.files").open(vim.uv.cwd(), true) end, desc = "Open mini.files (cwd)" },
+  }
+})
